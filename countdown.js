8ac20a663f6dd33203dc73723c49347546f99d4b
Jude Shorbaji <jude@Judes-MacBook-Pro.local>
2024-10-26 15:35:45 -0400

    added functionality to countdown

---
+++ countdown.js
@@ -0,0 +1,15 @@
+const startingMinutes =0;
+let time=startingMinutes*60;
+const countdownEL= document.getElementById('countdown');
+
+let refreshIntervalId = setInterval(updateCountdown,1000);
+
+function updateCountdown(){
+    const minutes=Math.floor(time/60)<10 ? "0"+Math.floor(time/60): Math.floor(time/60);
+    let seconds=time%60<10 ? "0"+time%60: time%60;
+    
+    countdownEL.innerHTML = minutes+":"+seconds;
+    time--;
+    time = time < 0 ? 0 : time; 
+}
+
