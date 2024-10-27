52ad5b57fe9ed09f3617455a1fbb002e11251ec9
rutvikarnik5 <rutvi.karnik@rutgers.edu>
2024-10-27 09:36:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,57 @@
-let array = [];
+let terms = [];
+
 let concept1;
 
 document.getElementById("Submit1").onclick = function(){
     concept1 = document.getElementById("Input1").value;
-    document.getElementById("H61").textContent = `${concept1}`
-    array.push(concept1);
+    terms.push(concept1);
+    document.getElementById("concept1").textContent = `${concept1}`
+    document.getElementById("Input1").value = '';
 }
 
 let concept2;
 
 document.getElementById("Submit2").onclick = function(){
     concept2 = document.getElementById("Input2").value;
-    document.getElementById("H62").textContent = `${concept2}`
-    array.push(concept2);
+    terms.push(concept2);
+    document.getElementById("concept2").textContent = `${concept2}`
+    document.getElementById("Input2").value = '';
 }
 
 let concept3;
 
 document.getElementById("Submit3").onclick = function(){
     concept3 = document.getElementById("Input3").value;
-    document.getElementById("H63").textContent = `${concept3}`
-    array.push(concept3);
+    terms.push(concept3);
+    document.getElementById("concept3").textContent = `${concept3}`
+    document.getElementById("Input3").value = '';
+}
+
+let definition = [];
+
+let def1;
+document.getElementById("Submit4").onclick = function(){
+    def1 = document.getElementById("Mnemonic1").value;
+    definition.push(def1);
+    document.getElementById("Mnemonic1").value = '';
+}
+
+let def2;
+document.getElementById("Submit5").onclick = function(){
+    def2 = document.getElementById("Mnemonic2").value;
+    definition.push(def2);
+    document.getElementById("Mnemonic2").value = '';
+}
+
+let def3;
+document.getElementById("Submit6").onclick = function(){
+    def3 = document.getElementById("Mnemonic3").value;
+    definition.push(def3);
+    document.getElementById("Mnemonic3").value = '';
+}
+
+document.getElementById("testKnowledge").onclick = function(){
+    def3 = document.getElementById("Mnemonic3").value;
+    definition.push(def3);
+    document.getElementById("Mnemonic3").value = '';
 }
