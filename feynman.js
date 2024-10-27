805d4c2d154f6495e3046d9bcfe112c2bde61e48
Jude Shorbaji <jude@Judes-MacBook-Pro.local>
2024-10-26 20:01:17 -0400

    feynmanUX iteration 1

--- feynman.js
+++ feynman.js
@@ -1,10 +1,13 @@
 const speechBubble= document.getElementById('speechBubble');//this variable is used to change the text of a textbubble 
-const text = ["Hi!", "I'm Sally", "ready to study?"];
-let stage=1;
+
+let stage=0;
+var form = document.getElementById("formId");
 function conversation(){
+    
     if (stage<text.length){
     speechBubble.innerHTML=text[stage];
     stage++;
     }
-    
+    return false;
+    document.getElementById("chat").reset();
 }
