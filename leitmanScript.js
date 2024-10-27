97fbbc20f3764da9b1e32dd8d9036733383eec29
amyal <amy.alphonse@outlook.com>
2024-10-27 11:53:13 -0400

    update letinman

--- leitmanScript.js
+++ leitmanScript.js
@@ -25,7 +25,7 @@
 
      //adds everything to an array
      allTerms = [term1.value, term2.value, term3.value, term4.value, term5.value];
-     allDefinitions = [term1.value, term2.value, term3.value, term4.value, term5.value];
+     allDefinitions = [definition1.value, definition2.value, definition3.value, definition4.value, definition5.value];
 
      console.log(allTerms);
      console.log(allDefinitions);
@@ -40,11 +40,11 @@
      document.getElementById("#term4").value = '';
      document.getElementById("#term5").value = '';
 
-     document.getElementById("#definition1").reset();
-     document.getElementById("#definition2").reset();
-     document.getElementById("#definition3").reset();
-     document.getElementById("#definition4").reset();
-     document.getElementById("#definition5").reset();
+     document.getElementById("#definition1").value = '';
+     document.getElementById("#definition2").value = '';
+     document.getElementById("#definition3").value = '';
+     document.getElementById("#definition4").value = '';
+     document.getElementById("#definition5").value = '';
 
  }
  
@@ -52,18 +52,26 @@
  function quizMe(){
      score = 0;
      document.getElementById("#definition1").value = '';
-     document.getElementById("#definition2").reset();
-     document.getElementById("#definition3").reset();
-     document.getElementById("#definition4").reset();
-     document.getElementById("#definition5").reset();
+     document.getElementById("#definition2").value = '';
+     document.getElementById("#definition3").value = '';
+     document.getElementById("#definition4").value = '';
+     document.getElementById("#definition5").value = '';
 
      alert("you have 45 seconds to answer:");
-     setTimeout(quizMe, 10000);
+     setTimeout(quizMe, 20000);
 
-     addToCardArray();
+    
+
      for (i = 5; i < 10; i++){
-
-         if (allDefinitions[i] == allDefinitions(i-5)){
+        // add to array
+        for (i = 0; i <5; i++){
+            let definition1 = document.querySelector("#definition1");
+            let definition2 = document.querySelector("#definition2");
+            let definition3 = document.querySelector("#definition3");
+            let definition4 = document.querySelector("#definition4");
+            let definition5 = document.querySelector("#definition5");
+        }
+        if (allDefinitions[i] == allDefinitions(i-5)){
              score = score + 1;
          }
 
